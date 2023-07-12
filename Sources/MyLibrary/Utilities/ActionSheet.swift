b3c547acd9b13bc71c2ab88f7b15162bb985ce0a
ashik11698 <mdashikur.rahman@bjitgroup.com>
2023-07-12 16:21:51 +0600

    Localization

--- Sources/MyLibrary/Utilities/ActionSheet.swift
+++ Sources/MyLibrary/Utilities/ActionSheet.swift
@@ -18,9 +18,7 @@ class ActionSheet {
     /// - Returns: returns UIAlertController to show sheet in viewController where the functions called
     func speedActionSheet(avPlayer: AVPlayer) -> UIAlertController {
         
-        //let title = NSLocalizedString("Speed", comment: "")
-        let title = NSLocalizedString("Speed", bundle: Bundle.module, comment: "a comment")
-        
+        let title = NSLocalizedString("Speed", comment: "")
         let selectAny = NSLocalizedString("Select any", comment: "")
         let normal = NSLocalizedString("Normal", comment: "")
         let cancel = NSLocalizedString("Cancel", comment: "")
